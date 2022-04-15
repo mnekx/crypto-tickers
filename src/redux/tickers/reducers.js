b7faaf85e9244cce12e340fd0a543b4114ccabe3
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-15 15:21:41 +0300

    Update reducers.js

--- src/redux/tickers/reducers.js
+++ src/redux/tickers/reducers.js
@@ -1,29 +1,39 @@
-import { FETCHING_STARTED, TICKERS_FETCHED, ERROR_FETCHING, TICKER_SEARCHED, STARTED_SEARCHING  } from './actionTypes'
+import {
+  FETCHING_STARTED,
+  TICKERS_FETCHED,
+  ERROR_FETCHING,
+  TICKER_SEARCHED,
+  STARTED_SEARCHING,
+} from './actionTypes';
 
 const defaultState = {
-    list: [],
-    loading: false,
-    error: false,
-    searching: false
-}
+  list: [],
+  loading: false,
+  error: false,
+  searching: false,
+};
 
-export const tickers = (prevState=defaultState, action) => {
-    switch (action.type) {
-        case FETCHING_STARTED:
-            return {...prevState, loading: true}
-        case TICKERS_FETCHED:
-            return {list: action.payload, loading: false, error: false}
-        case ERROR_FETCHING:
-            return {...prevState, error: true, loading: false}
-        case TICKER_SEARCHED:
-            console.log(action.name)
-            const re = new RegExp(`${action.name}`, 'img')
-            console.log({...prevState, searching: false, list: prevState.list.filter(ticker=>ticker.name.match(re) !== null)})
-            return {...prevState, searching: false, list: prevState.list.filter(ticker=>ticker.name.match(re) !== null)}
-        case STARTED_SEARCHING:
-            return {...prevState, searching: true}
-    
-        default:
-           return prevState;
-    }
-}
+const tickers = (prevState = defaultState, action) => {
+  const re = new RegExp(`${action.name}`, 'img');
+  switch (action.type) {
+    case FETCHING_STARTED:
+      return { ...prevState, loading: true };
+    case TICKERS_FETCHED:
+      return { list: action.payload, loading: false, error: false };
+    case ERROR_FETCHING:
+      return { ...prevState, error: true, loading: false };
+    case TICKER_SEARCHED:
+      return {
+        ...prevState,
+        searching: false,
+        list: prevState.list.filter((ticker) => ticker.name.match(re) !== null),
+      };
+    case STARTED_SEARCHING:
+      return { ...prevState, searching: true };
+
+    default:
+      return prevState;
+  }
+};
+
+export default tickers;
