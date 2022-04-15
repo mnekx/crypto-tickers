c9da797566e7f74f15b96d357943501b00aa8755
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-15 14:46:11 +0300

    Create tickers-service.js

---
+++ src/services/tickers-service.js
@@ -0,0 +1,12 @@
+import http from '../server/http-tickers';
+
+const getAll = () => http.get('/tickers');
+
+const get = (id) => http.get(`/tickers/${id}`);
+
+const TickersService = {
+  getAll,
+  get
+};
+
+export default TickersService;
