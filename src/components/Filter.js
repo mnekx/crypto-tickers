0d31e181192093d9616f306d06c5b60adbc940c5
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-15 15:21:01 +0300

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { startedSearching, tickerSearched } from '../redux/tickers/actions';
+
 const Filter = () => {
-    const dispatch = useDispatch()
-    const handleChange = (e) => {
-        dispatch(startedSearching())
-        dispatch(tickerSearched(e.target.value))
-    }
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    dispatch(startedSearching());
+    dispatch(tickerSearched(e.target.value));
+  };
   const handleSubmit = (e) => {
-      e.preventDefault()
-    console.log(e);
+    e.preventDefault();
   };
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       <label htmlFor="name">
-        <input type="text" onChange={(e)=>handleChange(e)} name="tickerName" id="name" placeholder="Filter..." />
+        <input type="text" onChange={(e) => handleChange(e)} name="tickerName" id="name" placeholder="Filter..." />
       </label>
     </form>
   );
