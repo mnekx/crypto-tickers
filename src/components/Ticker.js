5ff8229e4a684a30702453b7f68f0a67b18a7fb0
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-15 15:21:03 +0300

    Update Ticker.js

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,29 +1,42 @@
 import { NavLink } from 'react-router-dom';
-const Ticker = ({ ticker }) => {
-  return (
-    <li key={ticker.id} style={{
+import PropTypes from 'proptypes';
+
+const Ticker = ({ ticker }) => (
+  <li
+    key={ticker.id}
+    style={{
       backgroundImage:
-        'url("https://cryptoicons.org/api/icon/' +
-        ticker.symbol.toLowerCase() +
-        '/400")',
-    }} className="item-container imaged-bg">
-      <NavLink to={'tickers/' + ticker.id}>
-        
-        <section
-          
-          className="item">
-          <h2>{ticker.name}</h2>
-          <p>
-            <span>
-              <b>Supply: </b>
-            </span>
-            {ticker.total_supply.toLocaleString()}
-          </p>
-          <i class="far fa-arrow-alt-circle-right"></i>
-        </section>
-      </NavLink>
-    </li>
-  );
+        `url("https://cryptoicons.org/api/icon/${
+          ticker.symbol.toLowerCase()
+        }/400")`,
+    }}
+    className="item-container imaged-bg"
+  >
+    <NavLink to={`tickers/${ticker.id}`}>
+
+      <section
+        className="item"
+      >
+        <h2>{ticker.name}</h2>
+        <p>
+          <span>
+            <b>Supply: </b>
+          </span>
+          {ticker.total_supply.toLocaleString()}
+        </p>
+        <i className="far fa-arrow-alt-circle-right" />
+      </section>
+    </NavLink>
+  </li>
+);
+
+Ticker.propTypes = {
+  ticker: PropTypes.shape({
+    id: PropTypes.string,
+    symbol: PropTypes.string,
+    total_supply: PropTypes.string,
+    name: PropTypes.string,
+  }).isRequired,
 };
 
 export default Ticker;
