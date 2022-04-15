b393282e3a2a07841e6c6ee25cbaed352bb0674c
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-15 14:45:42 +0300

    Create actions.js

---
+++ src/redux/ticker/actions.js
@@ -0,0 +1,25 @@
+import TickersService from '../../services/tickers-service';
+import {
+  TICKER_RETRIEVED,
+  TICKER_ERROR_FETCHING,
+  TICKER_FETCHING_STARTED,
+} from './actionTypes';
+
+const tickerFetchingStarted = () => ({ type: TICKER_FETCHING_STARTED });
+
+const tickerFetched = (data) => ({ type: TICKER_RETRIEVED, payload: data });
+
+const tickerFetchingErrored = () => ({ type: TICKER_ERROR_FETCHING });
+
+export const tickerRetrieved = (id) => async (dispatch) => {
+  dispatch(tickerFetchingStarted());
+  try {
+    const res = await TickersService.get(id);
+
+    dispatch(tickerFetched(res.data));
+    return Promise.resolve(res.data);
+  } catch (error) {
+    dispatch(tickerFetchingErrored());
+    return Promise.resolve(error);
+  }
+};
