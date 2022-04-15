64b08b63d596054f922e7f30bb52b0cf2d3afb34
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-15 14:45:47 +0300

    Create reducers.js

---
+++ src/redux/ticker/reducers.js
@@ -0,0 +1,16 @@
+import { TICKER_ERROR_FETCHING, TICKER_FETCHING_STARTED, TICKER_RETRIEVED } from "./actionTypes";
+const defautState = { data: null, error: false, loading: false };
+
+export const selectedTicker = (prevState = defautState, action) => {
+  switch (action.type) {
+    case TICKER_FETCHING_STARTED:
+      return { ...prevState, loading: true };
+    case TICKER_RETRIEVED:
+      return { data: action.payload, loading: false, error: false };
+    case TICKER_ERROR_FETCHING:
+      return { ...prevState, error: true };
+    default:
+      return prevState;
+  }
+};
+
